449aa950215393f00a125f1a201a8b9f781796cb
Paul van Santen <pvansanten@transip.nl>
2017-04-03 11:54:42 +0200

    Use different colours for song times

--- util/utils.go
+++ util/utils.go
@@ -9,6 +9,12 @@ func FormatSongLength(duration time.Duration) (form string) {
 	minutes := int(duration.Minutes())
 	seconds := int(duration.Seconds()) - (minutes * 60)
 
+	if minutes > 60 {
+		hours := minutes / 60
+		minutes -= hours * 60
+		form = fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+		return
+	}
 	form = fmt.Sprintf("%02d:%02d", minutes, seconds)
 	return
 }
