fca36597c9a231312ae5327f8578b8faea228d84
Paul <pvansanten@gmail.com>
2017-04-07 09:37:39 +0200

    Announcement refining

--- bot/commands.go
+++ bot/commands.go
@@ -22,7 +22,7 @@ func (c *Command) execute(bot *MusicBot, event *irc.Event, parameters []string) 
 var HelpCommand = Command{
 	Name: "help",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		var names []string
 		for commandName := range bot.commands {
 			names = append(names, boldText(commandName))
@@ -35,7 +35,7 @@ var HelpCommand = Command{
 var WhitelistCommand = Command{
 	Name: "whitelist",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		realname := event.User
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, "!music whitelist <show|add|remove> [user]")
@@ -99,7 +99,7 @@ var WhitelistCommand = Command{
 var NextCommand = Command{
 	Name: "next",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		_, err := bot.player.Next()
 		if err != nil {
 			event.Connection.Privmsg(target, inverseText(err.Error()))
@@ -112,7 +112,7 @@ var NextCommand = Command{
 var PlayCommand = Command{
 	Name: "play",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		_, err := bot.player.Play()
 		if err != nil {
 			event.Connection.Privmsg(target, inverseText(err.Error()))
@@ -125,7 +125,7 @@ var PlayCommand = Command{
 var PauseCommand = Command{
 	Name: "pause",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		err := bot.player.Pause()
 		if err != nil {
 			event.Connection.Privmsg(target, inverseText(err.Error()))
@@ -144,7 +144,7 @@ var PauseCommand = Command{
 var StopCommand = Command{
 	Name: "stop",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		err := bot.player.Stop()
 		if err != nil {
 			event.Connection.Privmsg(target, inverseText(err.Error()))
@@ -156,7 +156,7 @@ var StopCommand = Command{
 var CurrentCommand = Command{
 	Name: "current",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		song, remaining := bot.player.GetCurrentSong()
 		if song != nil {
 			event.Connection.Privmsgf(target, "Current song: %s%s%s "+italicText("(%s remaining)"), BOLD_CHARACTER, formatSong(song), BOLD_CHARACTER, util.FormatSongLength(remaining))
@@ -169,7 +169,7 @@ var CurrentCommand = Command{
 var AddCommand = Command{
 	Name: "add",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, boldText("!music add <music link>"))
 			return
@@ -189,7 +189,7 @@ var AddCommand = Command{
 var OpenCommand = Command{
 	Name: "open",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, boldText("!music open <music link>"))
 			return
@@ -217,7 +217,7 @@ var ShuffleCommand = Command{
 var ListCommand = Command{
 	Name: "list",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		items := bot.player.GetQueuedSongs()
 		if len(items) == 0 {
 			event.Connection.Privmsg(target, italicText("The player is empty"))
@@ -245,7 +245,7 @@ var FlushCommand = Command{
 var SearchCommand = Command{
 	Name: "search",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, "!music search [<playerName>] <search term>")
 			return
@@ -271,7 +271,7 @@ var SearchCommand = Command{
 var SearchAddCommand = Command{
 	Name: "search-add",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, "!music search-add [<playerName>] <search term>")
 			return
@@ -320,7 +320,7 @@ var VolDownCommand = Command{
 var VolCommand = Command{
 	Name: "vol",
 	Function: func(bot *MusicBot, event *irc.Event, parameters []string) {
-		target, _ := getTarget(event)
+		target, _, _ := bot.getTarget(event)
 		if len(parameters) < 1 {
 			event.Connection.Privmsg(target, "!music vol <volume>")
 			return
