229059dd19993793eadc7665fb15a26664e38c99
Paul <pvansanten@gmail.com>
2017-05-31 21:39:32 +0200

    Refactor youtube meta/api class, Fix type searching for Youtube songplayer

--- bot/commands.go
+++ bot/commands.go
@@ -319,7 +319,7 @@ var SearchAddCommand = Command{
 		}
 		for plyr, res := range results {
 			for _, item := range res {
-				bot.announceMessagef(false, event, "%s added song: %s (%s)", boldText(event.Nick), formatSong(item), italicText(plyr))
+				bot.announceMessagef(false, event, "%s added song(s): %s (%s)", boldText(event.Nick), formatSong(item), italicText(plyr))
 				_, err := bot.player.AddSongs(item.GetURL())
 				if err != nil {
 					event.Connection.Privmsg(target, inverseText(err.Error()))
